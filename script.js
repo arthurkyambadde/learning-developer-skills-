099af4074d433b760ee42b11b8d1cfef74719588
iamarthur <arthurkyambadde9@gmail.com>
2022-05-12 20:27:33 +0300

    fixing bug

--- script.js
+++ script.js
@@ -30,8 +30,63 @@ const calcTempAmplitude = function (temps) {
     if (curTemp > max) max = curTemp;
     if (curTemp < min) min = curTemp;
   }
-  console.log(max);
-  console.log(min);
+  console.log(max, min);
+
+  return max - min;
 };
 
-calcTempAmplitude([1, 20, 501, 15, 10, "boy", 33, 400, 0, -20, 7]);
+const amplitude = calcTempAmplitude(temperature);
+
+console.log(amplitude);
+
+calcTempAmplitude([2, 5, 8, 9, 4, 99, -20]);
+
+console.log(amplitude);
+
+// function should receive two arrays
+// how to merge arrays
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+
+  return max - min;
+};
+
+const amplitudenew = calcTempAmplitudeNew([1, 3, 6], [4, 7, 9, 2]);
+
+console.log(amplitudenew);
+
+//learning  debugging
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+
+    // fix bug
+    //typeof value must be a value
+    // value: prompt("Degrees celsius:"),
+    value: Number(prompt("Degrees celsius:")),
+  };
+
+  // find bug
+  console.table(measurement);
+  console.log(typeof measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
